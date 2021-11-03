f3806cd561f9f59281bd269bb36eb3f0f25a3d3c
patrickodey <patrickodey419@gmail.com>
2021-11-03 07:21:33 +0100

    HashRouter

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ function App() {
             <Route exact path="/">
               <Hero/>
             </Route>
-            <Route path="/about">
+            <Route path="#/about">
               <About/>
             </Route>
-            <Route path="/contact">
+            <Route path="#/contact">
               <Contact/>
             </Route>
             <Route>
