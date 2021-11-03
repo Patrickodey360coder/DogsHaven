c9b769939cc0f9c99e90fe5a8f0ac7b14a5c96e2
patrickodey <patrickodey419@gmail.com>
2021-11-03 08:04:08 +0100

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {HashBrowser as Router} from 'react-router-dom';
 
 ReactDOM.render(
-    <Router>
       <App />
-    </Router>
 ,
   document.getElementById('root')
 );
